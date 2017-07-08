2ebbbb16cbd4e1e4bd47b2910ad0547f065220b7
Shchetinina Victoria <shchetinina.vika@mail.ru>
2017-07-08 13:14:28 +0300

    Добавлены сущности

---
+++ Epam.Shop/Epam.Shop.Entities/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Shop.Entities
+{
+    public class Book
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public int Year { get; set; }
+
+        public int Price { get; set; }
+    }
+}
--- Epam.Shop/Epam.Shop.Entities/User.cs
+++ Epam.Shop/Epam.Shop.Entities/User.cs
@@ -8,5 +8,19 @@ namespace Epam.Shop.Entities
 {
     public class User
     {
+        public Guid Id { get; set; }
+
+        public string Login { get; set; }
+
+        public byte[] Password { get; set; }
+
+        public string Name { get; set; }
+
+        public string SecondName { get; set; }
+
+        public string Email { get; set; }
+
+        public Guid IdRole { get; set; }
+
     }
 }
