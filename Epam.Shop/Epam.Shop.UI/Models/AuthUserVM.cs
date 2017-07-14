684cf2c620a8f2a6834ca7558c3e5b6164357b3d
Shchetinina Victoria <shchetinina.vika@mail.ru>
2017-07-14 19:52:03 +0300

    Сокрытие пароля при вводе

--- Epam.Shop/Epam.Shop.UI/Models/AuthUserVM.cs
+++ Epam.Shop/Epam.Shop.UI/Models/AuthUserVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Web.Security;
@@ -8,8 +9,13 @@ namespace Epam.Shop.UI.Models
 {
     public class AuthUserVM
     {
+        [Required]
+        [Display(Name = "Логин")]
         public string Login { get; set; }
 
+        [Required]
+        [Display(Name = "Пароль")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
 
         public static bool LogIn(AuthUserVM model)
